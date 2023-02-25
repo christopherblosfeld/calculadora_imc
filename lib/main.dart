16bc08b29f000788f7b2ec57d81fd0ee29ca94fc
christopherblosfeld <christopherblosfeld7@gmail.com>
2023-02-24 23:02:47 -0300

    Subindo a página completa do IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,164 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _textoinfo = 'Informe os dados para calcular.';
+  final _formKey = GlobalKey<FormState>();
+  final _pesoKey = GlobalKey<FormFieldState>();
+  final _alturaKey = GlobalKey<FormFieldState>();
+
+  void _resetFields() {
+    weightController.text = '';
+    heightController.text = '';
+    _textoinfo = 'Informe os dados para calcular.';
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _calculate() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      print(imc);
+
+      if (imc < 18.6) {
+        _textoinfo = 'Abaixo do Peso, IMC: ${imc.toStringAsPrecision(3)}';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _textoinfo = 'Peso ideal, IMC: ${imc.toStringAsPrecision(3)}';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _textoinfo =
+            'Levemente acima do peso, IMC: ${imc.toStringAsPrecision(3)}';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _textoinfo = 'Obesidade grau I, IMC: ${imc.toStringAsPrecision(3)}';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _textoinfo = 'Obesidade grau II, IMC: ${imc.toStringAsPrecision(3)}';
+      } else if (imc >= 40) {
+        _textoinfo =
+            'Obesidade grau III, c vai morrer !! IMC: ${imc.toStringAsPrecision(3)}';
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: AppBar(
+            title: Text('Calculadora de IMC'),
+            centerTitle: true,
+            backgroundColor: Colors.green,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: () {
+                  setState(() {
+                    _resetFields();
+                  });
+                },
+              )
+            ]),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+              child: Column(
+                  key: _formKey,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Icon(Icons.person_outline,
+                        size: 120, color: Colors.green),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      key: _pesoKey,
+                      validator: _validaPeso,
+                      maxLength: 3,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Peso (kg)',
+                        labelStyle: TextStyle(color: Colors.green),
+                      ),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.green, fontSize: 25),
+                      controller: weightController,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      key: _alturaKey,
+                      validator: _validaAltura,
+                      maxLength: 3,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Altura (cm)',
+                        labelStyle: TextStyle(color: Colors.green),
+                      ),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.green, fontSize: 25),
+                      controller: heightController,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green), //Cor verde do botao
+                        onPressed: () {
+                          if (_pesoKey.currentState?.validate() == false ||
+                              _alturaKey.currentState?.validate() == false) {
+                            return;
+                          }
+                          _calculate();
+                        },
+
+                        child: const Text(
+                          'Calcular',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      _textoinfo,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green, fontSize: 25),
+                    ),
+                  ]),
+            )));
+  }
+}
+
+String? _validaPeso(String? peso) {
+  if (peso == null || peso.isEmpty) {
+    return 'Informe um peso!';
+  }
+}
+
+String? _validaAltura(String? altura) {
+  if (altura == null || altura.isEmpty) {
+    return 'Informe uma altura!';
   }
 }
